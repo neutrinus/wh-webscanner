99570438e210ad9acccb66031f85d435291900a2
Tomek Czyż <tomekczyz@gmail.com>
2012-02-20 00:01:44 +0100

    check_spell: init

---
+++ webscanner/scanner/plugins/check_spell.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+__doc__='''
+Enchant backend: hunspell
+
+requirements.pip: pyenchant guess-language
+requirements.debian: enchant hunspell-*?
+
+algorithm
+---------
+
+- extract text from html
+- guess language
+- check spelling
+
+
+'''
