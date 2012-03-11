bbfae5c9919d1ff9cc3fbe49e2c45fc581fcc68f
Marek Hobler <marek@hobler.pl>
2012-03-11 16:19:58 +0100

    geoip init

--- webscanner/scanner/plugins/check_dns.py
+++ webscanner/scanner/plugins/check_dns.py
@@ -22,83 +22,75 @@ class PluginDNS(PluginMixin):
         from scanner.models import Results
         domain = urlparse(command.test.url).hostname
 
-        try:    
-        
-            try:
-                #A
-                records = ""
-                answers = dns.resolver.query(domain, 'A')
-                for rdata in answers:
-                    records += "A %s <br>"%(rdata.to_text() )
-                            
-                
-                res = Results(test=command.test,group = RESULT_GROUP.general, importance=5)
-                res.output_desc = unicode(_("A records (IPv4)") )
-                if len(answers) > 1:
-                    res.output_full = unicode(_("<p>Your nameserver returned %s A records: <code>%s</code></p>"%(len(answers),records ) ))
-                    res.status = RESULT_STATUS.success
-                elif len(answers) == 1:
-                    res.output_full = unicode(_("<p>Your nameserver returned %s A record: <code>%s</code></p> <p>Having multiple A records with different IP can load-balance traffic.</p>"%(len(answers),records ) ))
-                    res.status = RESULT_STATUS.success
-                else:
-                    res.output_full = unicode(_("<p>There are no A records for this domain! It means that nobody can reach your website.</p>" ))
-                    res.status = RESULT_STATUS.error         
-                res.save()
+        try:
+            #A
+            records = ""
+            answers = dns.resolver.query(domain, 'A')
+            for rdata in answers:
+                records += "A %s <br>"%(rdata.to_text() )
+                        
+            
+            res = Results(test=command.test,group = RESULT_GROUP.general, importance=5)
+            res.output_desc = unicode(_("A records (IPv4)") )
+            if len(answers) > 1:
+                res.output_full = unicode(_("<p>Your nameserver returned %s A records: <code>%s</code></p>"%(len(answers),records ) ))
+                res.status = RESULT_STATUS.success
+            elif len(answers) == 1:
+                res.output_full = unicode(_("<p>Your nameserver returned %s A record: <code>%s</code></p> <p>Having multiple A records with different IP can load-balance traffic.</p>"%(len(answers),records ) ))
+                res.status = RESULT_STATUS.success
+            else:
+                res.output_full = unicode(_("<p>There are no A records for this domain! It means that nobody can reach your website.</p>" ))
+                res.status = RESULT_STATUS.error         
+            res.save()
 
-                #check if all IP are public (non-private)
-                records = ""
-                for rdata in answers:
-                    if IP(rdata.address).iptype() == "PRIVATE":
-                        records += "%s <br"%rdate.address
-                
-                res = Results(test=command.test,group = RESULT_GROUP.general,importance=4)                
-                res.output_desc = unicode(_("No private IP in A records ") )
-                if not records:
-                    res.output_full = unicode(_("<p>All your A records are public.</p>" ))
-                    res.status = RESULT_STATUS.success
-                else:
-                    res.output_full = unicode(_("<p>Following A records for this domain are private: <code>%s</code>. Private IP can\'t be rached from the Internet. </p>"%(records) ))
-                    res.status = RESULT_STATUS.error    
-                res.save()
-                
-            except dns.resolver.NXDOMAIN:
-                res = Results(test=command.test,group = RESULT_GROUP.general, importance=5)
-                res.output_desc = unicode(_("A records (IPv4)") )
-                res.output_full = unicode(_("<p>Domain not found!.</p>" ))
-                res.status = RESULT_STATUS.error
-                res.save()
-            except StandardError,e:
-                log.exception("During check A record: %s"%str(e))
+            #check geolocation
+            from django.contrib.gis.utils import GeoIP
+            from settings import GEOIP_PATH
+            geoip = GeoIP()
+            locations = []
             
-            #try:
-                ##AAA
-                #records = ""
-                #answers = dns.resolver.query(domain, 'AAA')
-                #for rdata in answers:
-                    #records += "AAA %s <br>"%(rdata.to_text() )
-                            
+            for server in answers:
+                locations[server] = geoip.city(str(server))
                 
-                #res = Results(test=command.test)                
-                #res.output_desc = unicode(_("AAA records (IPv6)") )
-                #if len(answers) > 1:
-                    #res.output_full = unicode(_("<p>Your nameserver returned %s AAA records: <code>%s</code></p>"%(len(answers),records ) ))
-                    #res.status = RESULT_STATUS.success
-                #elif len(answers) == 1:
-                    #res.output_full = unicode(_("<p>Your nameserver returned %s AAA record: <code>%s</code></p> <p>Having multiple AAA records with different IP can load-balance traffic.</p>"%(len(answers),records ) ))
-                    #res.status = RESULT_STATUS.success
-                #else:
-                    #res.output_full = unicode(_("<p>There are no AAA records for this domain! It means that probably your site is not IPv6 compatibile.</p>" ))
-                    #res.status = RESULT_STATUS.warning         
-                #res.save()
+            res = Results(test=command.test,group = RESULT_GROUP.general, importance=1)
+            res.output_desc = unicode(_("Web server(s) geo-location %s")%(location)) 
+            res.output_full = unicode(_('<p>Estimated geolocation of server %s is %s(%s) </p><div id="demoMap" style="height:250px"></div><script>\
+map = new OpenLayers.Map("demoMap");\
+map.addLayer(new OpenLayers.Layer.OSM());\
+map.zoomToMaxExtent();\
+</script>'%(server,location['city'],location['country_name'] )))
+            res.status = RESULT_STATUS.info
+            res.save()
+
             
-            #except StandardError,e:
-                #log.exception("During check AAA record: %s"%str(e))
-                
+            #check if all IP are public (non-private)
+            records = ""
+            for rdata in answers:
+                if IP(rdata.address).iptype() == "PRIVATE":
+                    records += "%s <br"%rdate.address
             
-            return STATUS.success
+            res = Results(test=command.test,group = RESULT_GROUP.general,importance=4)                
+            res.output_desc = unicode(_("No private IP in A records ") )
+            if not records:
+                res.output_full = unicode(_("<p>All your A records are public.</p>" ))
+                res.status = RESULT_STATUS.success
+            else:
+                res.output_full = unicode(_("<p>Following A records for this domain are private: <code>%s</code>. Private IP can\'t be rached from the Internet. </p>"%(records) ))
+                res.status = RESULT_STATUS.error    
+            res.save()
+            
+        except dns.resolver.NXDOMAIN:
+            res = Results(test=command.test,group = RESULT_GROUP.general, importance=5)
+            res.output_desc = unicode(_("A records (IPv4)") )
+            res.output_full = unicode(_("<p>Domain not found!.</p>" ))
+            res.status = RESULT_STATUS.error
+            res.save()
         except StandardError,e:
-            log.exception("%s"%str(e))
-            return STATUS.exception
+            log.exception("During check A record: %s"%str(e))
+        
+        #TODO: check AAA (ipv6)
+        
+        return STATUS.success
 
 
 
