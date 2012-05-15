1770aeb1077306959dfae8988d6efbd2cc7046d2
Marek Hobler <marek@hobler.pl>
2012-05-15 08:13:58 +0200

    optiimg: move to class

--- webscanner/scanner/plugins/check_optiimg.py
+++ webscanner/scanner/plugins/check_optiimg.py
@@ -15,6 +15,7 @@ from time import sleep
 from datetime import date
 from plugin import PluginMixin
 from scanner.models import STATUS,RESULT_STATUS, RESULT_GROUP
+from settings import PATH_TMPSCAN
 from django.utils.translation import get_language
 from django.utils.translation import ugettext_lazy as _
 
@@ -25,7 +26,6 @@ class PluginOptiimg(PluginMixin):
     name = unicode(_('OptiIMG'))
     wait_for_download = True        
 
-    PATH_TMPSCAN = "/tmp/test2/"
 
     def identify(self, filename):
         test_command = 'identify -format %%m "%s"'%(filename)
@@ -48,7 +48,7 @@ class PluginOptiimg(PluginMixin):
         return PATH_TMPSCAN + ''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(24)) 
     
     def optimize_agif(self, filename):
-        file1 = gentmpfilename()
+        file1 = self.gentmpfilename()
         files = [filename,file1]
         
         command = 'gifsicle -O2 %s --output %s'%(filename,file1)
@@ -59,7 +59,7 @@ class PluginOptiimg(PluginMixin):
 
 
     def optimize_jpg(self, filename):
-        file1 = gentmpfilename()
+        file1 = self.gentmpfilename()
         files = [filename,file1]
         
         command = 'jpegtran -outfile %s -optimise -copy none %s'%(file1,filename)
@@ -67,7 +67,7 @@ class PluginOptiimg(PluginMixin):
         (output, stderrdata) = p.communicate()
 
         if os.path.getsize(filename) > 10000:
-            file2 = gentmpfilename()
+            file2 = self.gentmpfilename()
             files.append(file2)
             
             command = 'jpegtran -outfile %s -optimise -progressive %s'%(file2,file1)
@@ -77,8 +77,8 @@ class PluginOptiimg(PluginMixin):
         
         
     def optimize_png(self, filename):
-        file1 = gentmpfilename()
-        file2 = gentmpfilename()
+        file1 = self.gentmpfilename()
+        file2 = self.gentmpfilename()
         files = [filename,file1, file2]
 
     
@@ -93,13 +93,13 @@ class PluginOptiimg(PluginMixin):
         
     
     def optimize_gif(self, filename):
-        file1 = gentmpfilename()
+        file1 = self.gentmpfilename()
         #convert it to png and then optimize it as png
         command = 'convert %s png:%s'%(filename,file1)
         p = subprocess.Popen(shlex.split(command), stdout=subprocess.PIPE)
         (output, stderrdata) = p.communicate()
 
-        flist = optimize_png(file1)
+        flist = self.optimize_png(file1)
         
         flist.append(filename)
         
@@ -120,8 +120,6 @@ class PluginOptiimg(PluginMixin):
         domain = command.test.domain
         path = command.test.download_path
 
-        
-        
         log.debug("Recursive check image files size in %s "%(path))
         filesizes = dict()
 
@@ -136,41 +134,43 @@ class PluginOptiimg(PluginMixin):
                 
                 #filesizes[fpath] = 
                 
-                ftype = identify(fpath)
+                ftype = self.identify(fpath)
                 
                 if ftype == 'JPEG' :
-                    ofiles = optimize_jpg(fpath)
+                    ofiles = self.optimize_jpg(fpath)
                 elif ftype == 'PNG':
-                    ofiles = optimize_png(fpath)
+                    ofiles = self.optimize_png(fpath)
                 elif ftype == 'GIF':
-                    ofiles = optimize_gif(fpath)
+                    ofiles = self.optimize_gif(fpath)
                 elif ftype == 'AGIF':
-                    ofiles = optimize_agif(fpath)
+                    ofiles = self.optimize_agif(fpath)
                 else:
                     ofiles = None
 
                 if ofiles:
-                    sfile = select_smallest_file(ofiles)
+                    sfile = self.select_smallest_file(ofiles)
                     print("Optimized %s to %s"%(sfile,os.path.getsize(sfile) ))
                     
-                    #for ofile in ofiles[:1]:
-                        #os.remove(ofile)
+                    #remove not needed files
+                    for ofile in ofiles[:1]:
+                        if (ofile != sfile) and (ofile != fpath):
+                            os.remove(ofile)
 
                         
-        from scanner.models import Results
-        res = Results(test=command.test, group=RESULT_GROUP.performance,importance=2)
-        res.output_desc = unicode(_("Images (png) optimalization"))
+        #from scanner.models import Results
+        #res = Results(test=command.test, group=RESULT_GROUP.performance,importance=2)
+        #res.output_desc = unicode(_("Images (png) optimalization"))
         
-            res.status = RESULT_STATUS.warning
-            res.output_full = unicode(_("We analized %s files. Some of them may need optimalization: <code>%s</code>"%(fcounter,txtoutput)))
-        else:
-            res.status = RESULT_STATUS.success
-            if fcounter >0:
-                res.output_full = unicode(_("All %s png files looks optimized. Good!"%(fcounter)))
-            else:
-                res.status = RESULT_STATUS.info
-                res.output_full = unicode(_("No png files found."))
-        res.save()
+            #res.status = RESULT_STATUS.warning
+            #res.output_full = unicode(_("We analized %s files. Some of them may need optimalization: <code>%s</code>"%(fcounter,txtoutput)))
+        #else:
+            #res.status = RESULT_STATUS.success
+            #if fcounter >0:
+                #res.output_full = unicode(_("All %s png files looks optimized. Good!"%(fcounter)))
+            #else:
+                #res.status = RESULT_STATUS.info
+                #res.output_full = unicode(_("No png files found."))
+        #res.save()
         
         #as plugin finished - its success
         return STATUS.success
