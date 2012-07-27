c465822d2975bd3385629051c73b0c78e7927ecb
Marek Hobler <marek@hobler.pl>
2012-07-27 15:51:36 +0200

    login form url

--- webscanner/account/urls.py
+++ webscanner/account/urls.py
@@ -1,9 +1,11 @@
 # -*- encoding: utf-8 -*-
 from django.conf.urls.defaults import patterns, include, url
 
-urlpatterns = patterns('account.views',
-        url(r'^user/logout/?$', 'ulogout'),
+urlpatterns = patterns('',
+        url(r'^user/logout/?$', 'account.views.ulogout'),
+        url(r'^user/login/?$', 'django.contrib.auth.views.login'),
 )
+
 
 
 
--- webscanner/account/views.py
+++ webscanner/account/views.py
@@ -34,4 +34,3 @@ def ulogout(request):
 
     return redirect('/')
 
-
