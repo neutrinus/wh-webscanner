630b6a936c6de1e0cd21086eb7c8d2fce19ab0ea
Marek Hobler <marek@hobler.pl>
2012-08-09 15:04:58 +0200

    caching setup

--- webscanner/scanner/views.py
+++ webscanner/scanner/views.py
@@ -15,22 +15,26 @@ import json
 from logs import log
 from scanner.models import *
 from account.models import UserProfile
+from django.views.decorators.cache import cache_page
 
+@cache_page(60 * 15)
 def index(request):
 	return render_to_response('scanner/index.html', {}, context_instance=RequestContext(request))
 
+@cache_page(60 * 30)
 @render_to('terms.html')
 def terms(request):
     return dict()
 
+@cache_page(60 * 30)
 @render_to('about.html')
 def about(request):
     return dict()
 
+@cache_page(60 * 30)
 @render_to('pricing.html')
 def pricing(request):
     return dict()
-
 
 @login_required
 @render_to('scanner/scan_archive.html')
@@ -101,15 +105,13 @@ def results(request):
     else:
         return redirect(reverse('scanner_index'))
 
-
+@cache_page(60 * 5)
 def show_report(request, uuid):
-
     try:
         test = Tests.objects.filter(uuid=uuid).get()
     except ObjectDoesNotExist:
         messages.warning(request, _('Report not found, please check ID and URL'))
         return redirect('/')
-
     return render_to_response('scanner/results.html', {'test': test}, context_instance=RequestContext(request))
 
 def check_results(request, uuid):
--- webscanner/settings.py
+++ webscanner/settings.py
@@ -53,6 +53,10 @@ CACHES = {
     }
 }
 
+#CACHE_MIDDLEWARE_KEY_PREFIX = ""
+#CACHE_MIDDLEWARE_SECONDS = 60*5
+#CACHE_MIDDLEWARE_ALIAS = "default"
+
 TIME_ZONE = 'Europe/Warsaw'
 
 LANGUAGE_CODE = 'en'
@@ -102,14 +106,15 @@ TEMPLATE_LOADERS = (
 )
 
 MIDDLEWARE_CLASSES = (
+    #'django.middleware.cache.UpdateCacheMiddleware',
     'django.middleware.gzip.GZipMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'djangosecure.middleware.SecurityMiddleware',
-
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    #'django.middleware.cache.FetchFromCacheMiddleware',
 )
 
 TEMPLATE_CONTEXT_PROCESSORS = [
