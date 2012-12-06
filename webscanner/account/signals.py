99f434e294697010449dac7bcf3a0a64c03bf76b
Tomek Czyż <tomekczyz@gmail.com>
2012-12-06 10:25:19 +0100

    account: parially adjust starting test using new 'start'

--- webscanner/account/signals.py
+++ webscanner/account/signals.py
@@ -10,11 +10,12 @@ def create_test_during_login(sender, request, user, **kwargs):
     if url is None:
         return
     log.info('creating test during login for %r'%user)
-    test = Tests.create_from_signed_url(url, user)
-    if test:
+    try:
+        test = Tests.make_from_signed_url(url, user, request.META['REMOTE_ADDR'])
+        test.save()
         test.start()
         log.info('test created during login (%r) for %r'%(test, user))
-    else:
-        log.warning('problem with creating test during login: user:%r, sign:%r'%(user, url))
+    except Tests.StartError:
+        log.exception('problem with creating test during login: user:%r, sign:%r'%(user, url))
 
 user_logged_in.connect(create_test_during_login)
