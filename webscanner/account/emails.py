b4d518eb4d5eb76e135ec1f541972b227ad3ba22
Marek Hobler <marek@hobler.pl>
2012-12-06 15:02:01 +0100

    fix welcome email title

--- webscanner/account/emails.py
+++ webscanner/account/emails.py
@@ -11,7 +11,7 @@ def send_welcome_email(sender, user, request, **kwargs):
     try:
         template = loader.find_template('account/email/welcome.html')[0]
         text = template.render(Context({'user':user}))
-        user.email_user(_('[https://webcheck.me] Welcome'), text, "support@webcheck.me")
+        user.email_user(_('[webcheck.me] Welcome'), text, "support@webcheck.me")
         log.info('Welcome mail sent to user: %s'%(user.email))
     except Exception:
         log.exception('Error sending welcome mail to user: %s'%(user.email))
