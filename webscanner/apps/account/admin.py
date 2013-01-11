7910fe8518fb93bcfbbd8d13925fa686153010e3
Tomek Czyż <tomekczyz@gmail.com>
2013-01-11 15:55:25 +0100

    account: add link to user tests in admin

--- webscanner/apps/account/admin.py
+++ webscanner/apps/account/admin.py
@@ -1,4 +1,5 @@
 
+from django.core.urlresolvers import reverse
 from django.contrib import admin
 from django.contrib.auth.models import User
 from django.contrib.auth.admin import UserAdmin
@@ -13,13 +14,17 @@ class UserProfileInlineAdmin(admin.StackedInline):
 class UserAdmin(UserAdmin):
     inlines = UserAdmin.inlines + [ UserProfileInlineAdmin ]
     fieldsets = UserAdmin.fieldsets
-    list_display = ('pk','email', 'is_active','credits','last_login','date_joined', 'is_staff','is_superuser')
+    list_display = ('pk','email', 'is_active','credits','last_login','date_joined', 'is_staff','is_superuser', 'user_tests_link')
     list_display_links = ('pk','email')
     ordering = ('date_joined',)
 
     def credits(self, user):
         return user.userprofile.credits
     credits.admin_order_field = 'userprofile__credits'
+
+    def user_tests_link(self, user):
+        return '<a href="%s?user=%s" target="_blank">tests</a>' % (reverse('admin:scanner_tests_changelist'), user.pk)
+    user_tests_link.allow_tags = True
 
 
 admin.site.unregister(User)
