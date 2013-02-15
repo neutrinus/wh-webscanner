1e4db53fcb72bd0b4ea0ad013da1af75e547244d
Tomasz Czyż <tomasz.czyz@whitehats.pl>
2013-02-15 15:57:05 +0100

    check_dns_mail: pep8

--- webscanner/apps/scanner/plugins/check_dns_mail.py
+++ webscanner/apps/scanner/plugins/check_dns_mail.py
@@ -33,7 +33,7 @@ class PluginDNSmail(PluginMixin):
             mxes = ""
             answers = dns.resolver.query(domain, 'MX')
             for rdata in answers:
-                mxes += "MX %s <br>"%(rdata.to_text() )
+                mxes += "MX %s <br>" % (rdata.to_text())
                 #print 'Host', rdata.exchange, 'has preference', rdata.preference
 
             #check if all IP are public (non-private)
@@ -46,16 +46,14 @@ class PluginDNSmail(PluginMixin):
                 for ip in mxips:
                     #check if address is not private
                     if IP(ip.address).iptype() == "PRIVATE":
-                        records += "%s %s <br>"%(mxdata.exchange,ip)
+                        records += "%s %s <br>" % (mxdata.exchange, ip)
 
                     #check if ip resolves intro FQDN - needed for email
                     try:
-                        mx_dnsname = dns.resolver.query(dns.reversename.from_address(ip.address),"PTR")
-                        reversemxes += "%s(%s): %s <br />"%(mxdata.exchange,ip.address,mx_dnsname[0])
+                        mx_dnsname = dns.resolver.query(dns.reversename.from_address(ip.address), "PTR")
+                        reversemxes += "%s(%s): %s <br />" % (mxdata.exchange, ip.address, mx_dnsname[0])
                     except dns.resolver.NXDOMAIN:
-                        noreversemxes += "%s(%s)<br />"%(mxdata.exchange,ip.address)
-
-
+                        noreversemxes += "%s(%s)<br />" % (mxdata.exchange, ip.address)
 
             #check geolocation
             locations = {}
@@ -72,30 +70,29 @@ class PluginDNSmail(PluginMixin):
             del res
 
             #check private IPs
-            res = Results(test=command.test,group = RESULT_GROUP.mail,importance=5)
-            res.output_desc = unicode(_("No private IP in MX records ") )
+            res = Results(test=command.test, group=RESULT_GROUP.mail, importance=5)
+            res.output_desc = unicode(_("No private IP in MX records "))
             if not records:
-                res.output_full = unicode(_("<p>All your MX records are public.</p>" ))
+                res.output_full = unicode(_("<p>All your MX records are public.</p>"))
                 res.status = RESULT_STATUS.success
             else:
-                res.output_full = unicode(_("<p>Following MX records for this domain are private: <code>%s</code>. Private IP can\'t be rached from the Internet. </p>"%(records) ))
+                res.output_full = unicode(_("<p>Following MX records for this domain are private: <code>%s</code>. Private IP can\'t be rached from the Internet. </p>" % (records)))
                 res.status = RESULT_STATUS.error
             res.save()
             del res
 
-            res = Results(test=command.test,group = RESULT_GROUP.mail,importance=3)
-            res.output_desc = unicode(_("Reverse Entries for MX records") )
+            res = Results(test=command.test, group=RESULT_GROUP.mail, importance=3)
+            res.output_desc = unicode(_("Reverse Entries for MX records"))
             if not noreversemxes:
-                res.output_full = unicode(_("<p>All your MX records have reverse records: <code>%s</code></p>"%(reversemxes) ))
+                res.output_full = unicode(_("<p>All your MX records have reverse records: <code>%s</code></p>" % (reversemxes)))
                 res.status = RESULT_STATUS.success
             else:
-                res.output_full = unicode(_("<p>Following MX records for dont have reverse entries: <code>%(noreversemxes)s</code>. Folowing MX records have reverse entries: <code>%(reversemxes)s</code>. </p>" % { "noreversemxes" : noreversemxes, "reversemxes" : reversemxes} ))
+                res.output_full = unicode(_("<p>Following MX records for dont have reverse entries: <code>%(noreversemxes)s</code>. Folowing MX records have reverse entries: <code>%(reversemxes)s</code>. </p>" % {"noreversemxes": noreversemxes, "reversemxes": reversemxes}))
                 res.status = RESULT_STATUS.error
 
             res.output_full += unicode(_("<p>All mail servers should have a reverse DNS (PTR) entry for each IP address (RFC 1912). Missing reverse DNS entries will make many mailservers reject your e-mails or mark them as SPAM. </p>"))
             res.save()
             del res
-
 
             spfrecord = ""
             try:
@@ -106,13 +103,13 @@ class PluginDNSmail(PluginMixin):
             except dns.resolver.NoAnswer:
                 pass
 
-            res = Results(test=command.test, group = RESULT_GROUP.mail, importance=2)
-            res.output_desc = unicode(_("SPF records") )
+            res = Results(test=command.test, group=RESULT_GROUP.mail, importance=2)
+            res.output_desc = unicode(_("SPF records"))
 
             res.output_full = "<p>Sender Policy Framework (SPF) is an email validation system designed to prevent email spam by detecting email spoofing, a common vulnerability, by verifying sender IP addresses. SPF allows administrators to specify which hosts are allowed to send mail from a given domain by creating a specific SPF record (or TXT record) in the Domain Name System (DNS). <a href='http://en.wikipedia.org/wiki/Sender_Policy_Framework'>More at wikipedia</a></p>"
 
             if spfrecord:
-                res.output_full += unicode(_("<p>OK, you have SPF record defined: <code>%s</code></p>"%(spfrecord) ))
+                res.output_full += unicode(_("<p>OK, you have SPF record defined: <code>%s</code></p>"%(spfrecord)))
                 res.status = RESULT_STATUS.success
             else:
                 res.output_full += unicode(_("<p>There is no SPF defined for your domain. Consider creating one - it helps a lot dealing with SPAM.</p>"))
@@ -122,7 +119,7 @@ class PluginDNSmail(PluginMixin):
 
             return STATUS.success
         except dns.resolver.Timeout,e:
-            res = Results(test=command.test,group = RESULT_GROUP.general,importance=3)
+            res = Results(test=command.test, group=RESULT_GROUP.general, importance=3)
             res.output_desc = unicode(_("MX records") )
             res.output_full = unicode(_("<p>There was timeout while asking your nameservers for MX records.</p>" ))
             res.status = RESULT_STATUS.error
