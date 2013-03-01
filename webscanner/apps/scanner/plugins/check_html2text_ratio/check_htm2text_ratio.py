62bb4b1a558279cc7fb7fdca27cdc8707665da9c
Tomasz Czyż <tomasz.czyz@whitehats.pl>
2013-03-01 12:11:40 +0100

    html2text: fix to new files list arch

--- webscanner/apps/scanner/plugins/check_html2text_ratio/check_htm2text_ratio.py
+++ webscanner/apps/scanner/plugins/check_html2text_ratio/check_htm2text_ratio.py
@@ -1,23 +1,13 @@
 #! /usr/bin/env python
 # -*- encoding: utf-8 -*-
-import sys
 import os
-import sys
-import bs4
-import mimetypes
-import glob
 
-from django.utils.translation import get_language
 from django.utils.translation import ugettext_lazy as _
-from django.template import Template, Context
-import dns.resolver
-import dns.reversename
-from IPy import IP
-import smtplib
 
 from scanner.plugins.plugin import PluginMixin
-from scanner.models import STATUS, RESULT_STATUS,RESULT_GROUP
-from addonsapp.tools import strip_html_tags
+from scanner.models import STATUS, RESULT_STATUS, RESULT_GROUP
+from webscanner.utils.html import clean_html
+
 
 class PluginHtml2TextRatio(PluginMixin):
     name = unicode(_('Code to text ratio'))
@@ -34,37 +24,42 @@ class PluginHtml2TextRatio(PluginMixin):
         chars_text = 0.0
         path = str(command.test.download_path)
 
-        for dir in glob.glob(os.path.join(path,'*%s*'%command.test.domain())):
-            for root, dirs, files in os.walk(str(dir)):
-                for file in files:
-                    file_path = os.path.abspath(os.path.join(root, file))
-                    if 'html' not in str(mimetypes.guess_type(file_path)[0]):
-                        continue
-                    self.log.debug('analizing file %s' % file_path)
-                    with open(file_path, 'r') as f:
-                        orig = f.read()
+        for file_info in command.test.downloaded_files:
+            file_path = os.path.join(path, file_info['path'])
 
-                        chars_total += len(str(orig))
-                        chars_text +=  len('\n'.join(strip_html_tags(bs4.BeautifulSoup(orig))))
-                        #print "file: %s; (%s / %s)" % (file_path, chars_text, chars_total)
+            if 'html' not in file_info['mime']:
+                continue
 
-        if chars_total:
+            self.log.debug('analizing file %s' % file_path)
+            try:
+                with open(file_path, 'r') as f:
+                    orig = f.read()
+
+                    chars_total += len(orig)
+                    chars_text += len(clean_html(orig))
+            except (IOError, OSError) as error:
+                self.log.warning('error in processing %s: %s' % (file_path, error))
+                continue
+
+        try:
             ratio = (chars_text / chars_total) * 100
-        else:
+        except ZeroDivisionError:
             ratio = 0.0
 
-        res = Results(test=command.test, group = RESULT_GROUP.seo, importance=2)
+        if ratio == 0.0:
+            self.log.warning('probably something goes wrong')
+            return STATUS.unsuccess
+
+        res = Results(test=command.test, group=RESULT_GROUP.seo, importance=2)
         res.output_desc = unicode(_("Code to text ratio "))
         res.output_full = unicode(_("<p>The code to text(content) ratio represents the percentage of actual text in your web page. Our software extracts content from website and computes the ratio. Some SEO folks say that perfect ratio should be between 20% and 70%, the others say that this parameter has little to none influence on SEO.</p>"))
         if ratio > 20 and ratio < 70:
             res.status = RESULT_STATUS.success
-            res.output_full += unicode(_("<p>Good, your code to text ratio is  <b>%.1f%%</b></p>"%(ratio ) ))
+            res.output_full += unicode(_("<p>Good, your code to text ratio is  <b>%.1f%%</b></p>" % ratio))
         else:
             res.status = RESULT_STATUS.warning
-            res.output_full += unicode(_("<p>Your code to text ratio is <b>%.1f%%</b>, maybe you could review content on your website and work on the copy?</p>"%(ratio)))
+            res.output_full += unicode(_("<p>Your code to text ratio is <b>%.1f%%</b>, maybe you could review content on your website and work on the copy?</p>" % ratio))
 
         res.save()
 
         return STATUS.success
-
-
